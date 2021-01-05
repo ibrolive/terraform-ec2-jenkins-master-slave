f69bedf6df4522a9d1f0fcb45f910a57e464791c
Ibrahim Dauda <ibrolive@gmail.com>
2021-01-05 16:09:54 +0400

    added outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "id" {
   description = "List of IDs of instances"
-  value       = aws_instance.this.*.id
+  value       = aws_instance.jenkins_master.*.id
 }
 
 output "arn" {
   description = "List of ARNs of instances"
-  value       = aws_instance.this.*.arn
+  value       = aws_instance.jenkins_master.*.arn
 }
 
 output "password_data" {
   description = "List of Base-64 encoded encrypted password data for the instance"
-  value       = aws_instance.this.*.password_data
+  value       = aws_instance.jenkins_master.*.password_data
 }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.this.*.public_dns
+  value       = aws_instance.jenkins_master.*.public_dns
 }
 
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = aws_instance.this.*.public_ip
+  value       = aws_instance.jenkins_master.*.public_ip
 }

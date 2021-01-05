12900e92f1bfe77425e86878b35fe60638f50134
Ibrahim Dauda <ibrolive@gmail.com>
2021-01-05 16:03:13 +0400

    added terraform script

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "jenkins_master" {
     instance_type               = "t2.micro"
     key_name                    = "JenkinsKeyPair"
     associate_public_ip_address = true
-    vpc_security_group_ids      = ["sg-0f89322a79aa4a404"] # ensure this Security Group has port 9091 opened
+    vpc_security_group_ids      = [module.http_sg.this_security_group_id] #["sg-0f89322a79aa4a404"] # ensure this Security Group has port 9091 opened
     user_data                   = templatefile("${path.cwd}/master-bootstrap.tmpl", {})
 
     tags = {
@@ -29,11 +29,16 @@ resource "aws_instance" "jenkins_slave" {
     instance_type               = "t2.micro"
     key_name                    = "JenkinsKeyPair"
     associate_public_ip_address = true
-    vpc_security_group_ids      = ["sg-0f89322a79aa4a404"] # ensure this Security Group has port 9091 opened
+    vpc_security_group_ids      = [module.http_sg.this_security_group_id] #["sg-0f89322a79aa4a404"] # ensure this Security Group has port 9091 opened
     user_data                   = templatefile("${path.cwd}/slave-bootstrap.tmpl", {})
 
     tags = {
         Name            = "Jenkins-Slave-${count.index+1}"
         ProvisionedBy   = "Terraform"
     }
+}
+
+resource "aws_key_pair" "JenkinsKeyPair" {
+  key_name   = "JenkinsKeyPair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 ${var.email}"
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "email" {
+  description = "Email to use in the key pair"
+  type        = string
+  default     = "email@domain.com"
+}
